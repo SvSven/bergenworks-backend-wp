b8c5fd219969f5b39f17fb90920113f3fb829648
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-10-01 18:17:19 +0200

    add acf options page values to site-info api endpoint

--- includes/api/v1/_getSiteInfo.php
+++ includes/api/v1/_getSiteInfo.php
@@ -1,10 +1,13 @@
 <?php
 
 function getSiteInfo($request) {
+
+    $acf_options = get_fields('options');
+
     $data = [
         "site_name" => get_bloginfo("name"),
         "site_description" => get_bloginfo("description")
     ];
 
-    return new WP_REST_Response($data, 200);
+    return new WP_REST_Response(array_merge($data, $acf_options), 200);
 }
