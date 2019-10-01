09ec1be409301cf81074de3810d2dd366c4f7f54
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-10-01 16:37:46 +0200

    restructure functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once('includes/api/v1/endpoints.php');
-
 function register_menus() {
     register_nav_menu('main-menu', ('Main menu'));
 }
-
 add_action('init', 'register_menus');
 
+require_once('includes/api/v1/endpoints.php');
 require_once("includes/acf-options.php");
