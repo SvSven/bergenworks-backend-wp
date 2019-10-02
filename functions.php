6399425e3e6f824a9b8c91f363250d07909b4848
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-10-02 22:13:45 +0200

    register footer menus

--- functions.php
+++ functions.php
@@ -2,6 +2,9 @@
 
 function register_menus() {
     register_nav_menu('main-menu', ('Main menu'));
+    register_nav_menu('footer-left', ('Footer menu left'));
+    register_nav_menu('footer-center', ('Footer menu center'));
+    register_nav_menu('footer-right', ('Footer menu right'));
 }
 add_action('init', 'register_menus');
 
