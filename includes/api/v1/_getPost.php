7f0bdafab72fc8a00bd17c4b4fe95db1d102b7f6
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-10-04 20:10:08 +0200

    add wrapper function/endpoint to retrieve specific post

---
+++ includes/api/v1/_getPost.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Retrieve page by slug
+ * Wrapper for built in WordPress endpoint: /wp-json/wp/v2/pages?slug=<name>
+ */
+function getPost($request) {
+    $slug = $request["slug"];
+
+    $id = null;
+
+    $post = get_page_by_path($slug, OBJECT, "post");
+    $id = $post->ID;
+
+    if (!$id) {
+        return new WP_Error('empty_category', 'could not find requested post', array('status' => 404));
+    }
+
+    $post_request  = new WP_REST_Request('GET', '/wp/v2/posts/' . $id);
+    $response = rest_do_request($post_request);
+
+    if ($response->is_error()) {
+        return new WP_Error('empty_category', 'error retrieving post', array('status' => 500));
+    }
+
+    return new WP_REST_Response($response->get_data(), 200);
+}
--- includes/api/v1/endpoints.php
+++ includes/api/v1/endpoints.php
@@ -4,6 +4,7 @@ require_once('_getFrontpage.php');
 require_once('_getSiteInfo.php');
 require_once('_getMenus.php');
 require_once('_getPage.php');
+require_once('_getPost.php');
 
 $endpoints = [
     [
@@ -25,6 +26,10 @@ $endpoints = [
     [
         "endpoint" => "page/(?P<slug>[a-zA-Z0-9-]+)",
         "callback" => "getPage"
+    ],
+    [
+        "endpoint" => "post/(?P<slug>[a-zA-Z0-9-]+)",
+        "callback" => "getPost"
     ]
 ];
 
