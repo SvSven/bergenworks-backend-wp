633d03cea98a6e4e17ddd4d60d29aa4ca09bf320
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-10-04 18:46:35 +0200

    fix issue with api url being incorrect when using site address setting

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * For some reason, WordPress has decided to use the value of the "Site Address" setting instead of
+ * the value of "WordPress Address" to create the rest API url. This, obviously, causes problems when
+ * using WordPress as a headless CMS. This fixes that.
+ */
+add_filter('rest_url', function($url) {
+    $url = str_replace(home_url(), site_url(), $url);
+    return $url;
+});
+
 function register_menus() {
     register_nav_menu('main-menu', ('Main menu'));
     register_nav_menu('footer-left', ('Footer menu left'));
