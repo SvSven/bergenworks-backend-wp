0ac58c2b6eeb7f3152d7aed6c2c55a82df5bcac0
Sven van Steenis <sven.vsteenis@gmail.com>
2019-11-10 20:30:43 +0100

    fix temp: convert home hero image rest field value
    Ideally we want this to be done by ACF itself because hardcoding field
    names and paths is not a good idea. May have to create a custom ACF
    field like the page_link field

--- includes/api/v1/custom-rest-fields.php
+++ includes/api/v1/custom-rest-fields.php
@@ -12,7 +12,21 @@ add_action( 'rest_api_init', function () {
     if (function_exists('get_fields')) {
         register_rest_field( ['page', 'post'], 'acf_content', array(
             'get_callback' => function($post) {
-                return get_fields($post['id']);
+
+                $fields = get_fields($post['id']);
+
+                if (get_option('page_on_front') == $post['id']) {
+                    /**
+                     * Todo: Have ACF return this specific image url so that we don't have
+                     * to hard code the fields here
+                     */
+                    $img_id = get_field('hero_content_image', $post['id']);
+                    $fields['hero']['content']['image'] = wp_get_attachment_image_url($img_id, 'page_hero');
+
+                    return $fields;
+                }
+
+                return $fields;
             }
         ));
     }
